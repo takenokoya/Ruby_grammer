cab42e448124d0b214e4708b64cbcc48d845a8a3
takenokoya <takuya1002@gmail.com>
2020-05-17 01:29:58 +0900

    add proc sample

--- sample_block.rb
+++ sample_block.rb
@@ -56,3 +56,18 @@ repeat_proc = Proc.new { |text| (text + "\n") * 2 }
 # repeat_proc = proc { |text| (text + "\n") * 2 }
 # Procオブジェクトをブロック引数として渡すときはブロックを渡すときと同様に & をつける
 greeting(&repeat_proc)
+
+# 3種類のprocオブジェクトを受取り、それぞれのあいさつに適用するgreetingメソッド
+# ブロック引数(&block)は1つまでしか渡せないかつ最後に渡さなければならない制約があるが、procオブジェクトを普通の引数として渡す分には制約はない
+def greeting_proc(proc1, proc2, proc3)
+  puts proc1.call('おはよう')
+  puts proc2.call('こんにちは')
+  puts proc3.call('こんばんは')
+end
+
+shuffle_porc = Proc.new { |text| text.chars.shuffle.join }
+repeat_proc = Proc.new { |text| text * 2 }
+question_proc = Proc.new { |text| "#{text}?"}
+
+# 3種類のprocオブジェクトをmethodにわたす
+greeting_proc(shuffle_porc, repeat_proc, question_proc)
