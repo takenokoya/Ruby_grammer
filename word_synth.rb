28942ff0ffb9cf972d043f9bba1fdfb77f72f23b
takenokoya <takuya1002@gmail.com>
2020-05-17 01:58:35 +0900

    add primitive word_synth

--- word_synth.rb
+++ word_synth.rb
@@ -1,1 +1,29 @@
+class WordSynth
+  def initialize
+    @effects = []
+  end
 
+  def add_effect(effect)
+    @effects << effect
+  end
+
+  def play(original_words)
+    @effects.inject(original_words) do |words, effect|
+      effect.call(words)
+    end
+  end
+
+  module Effects
+    def self.reverse
+      # procオブジェクトをメソッドの戻り値にする
+      ->(words) { words.split(' ').map(&:reverse).join(' ') }
+    end
+  end
+
+  synth = WordSynth.new
+  puts synth.play('Ruby is fun!')
+
+  # リバースエフェクトを適用
+  synth.add_effect(Effects.reverse)
+  puts synth.play('Ruby is fun!')
+end
